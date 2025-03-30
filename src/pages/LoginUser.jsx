9a8cf036226a302b465f2fe1c7ec3aeb4f78ef8f
Dhruv <dhruv.gautam.0022@gmail.com>
2025-03-30 14:47:42 +0530

    style user list page

--- src/pages/LoginUser.jsx
+++ src/pages/LoginUser.jsx
@@ -20,33 +20,44 @@ const LoginUser = () => {
     }
   };
   return (
-    <>
-      <div>
-        <h1>Login Page</h1>
+    <div className=" flex justify-center items-start mt-8 h-screen  w-screen">
+      <div className="h-1/3 w-1/3 p-4">
+        <h1 className=" text-center text-green-500 border-black border-b-4 mb-8 text-3xl font-bold p-2">
+          Login Page
+        </h1>
         {error && <p>{error}</p>}
-        <form onSubmit={handleLogin}>
-          <div className="emailInput ">
-            <label>Email</label>
+        <form onSubmit={handleLogin} className="form">
+          <div className="text-lg font-semibold p-2">
+            <label className="w-1/3">Email:</label>
             <input
+              className="input border-b-2 border-black w-2/3 ml-10 mb-4 p-1"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
-          <div>
+          <div className="text-lg font-semibold p-2">
             <label>Password:</label>
             <input
+              className="input border-b-2 border-black w-2/3 ml-2 mb-4 p-1"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
-          <button type="submit">Login</button>
+          <div className="flex items-center justify-center ">
+            <button
+              className="px-2 py-1 bg-green-500 text-white rounded-md"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "/src/styles/UserList.css";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -61,48 +60,71 @@ const UserList = () => {
   };
 
   return (
-    <div className="container">
-      <h1 className="header">UserList</h1>
-      <div className="userGrid">
-        {users.map((user) => (
-          <div key={user.id} className="card">
-            <img src={user.avatar} alt="avatar" className="avatar" />
-            <div>
-              <h3>
-                {user.fistName}
-                {user.LastName}
-              </h3>
-              <span>{`${user.first_name} ${user.last_name}`}</span>
+    <div className=" h-screen w-screen border-black border-2 bg-green-500">
+      <h1 class="mb-4 text-center mt-20  text-4xl font-extrabold   md:text-5xl lg:text-6xl dark:text-white">
+        UserList
+      </h1>
+      <div className="  mt-16  flex justify-center items-center ml-auto mr-auto">
+        <div className="grid grid-cols-3  gap-6 ">
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className="card flex gap-4 box-border bg-white p-6 shadow-xl rounded-lg  "
+            >
+              <img
+                src={user.avatar}
+                alt="avatar"
+                class="w-[150px] h-[150px]  p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+              />
 
-              <p>Email: {user.email}</p>
-              <div className="Buttons">
-                <button
-                  className="Button"
-                  onClick={() => {
-                    handleEdit(user.id);
-                  }}
-                >
-                  Edit
-                </button>
-                <button
-                  className="Button"
-                  onClick={() => {
-                    handleDelete(user.id);
-                  }}
-                >
-                  Delete
-                </button>
+              <div className="personal_details p-1 flex flex-col items-start justify-center">
+                <div className="name text-xl font-bold">
+                  Name:
+                  <span className="text-lg font-medium ml-1">
+                    {`${user.first_name} ${user.last_name}`}
+                  </span>
+                </div>
+                <div className="text-xl font-bold">
+                  Email:
+                  <span className="text-lg font-medium ml-2 ">
+                    {user.email}
+                  </span>
+                </div>
+
+                <div className="Buttons   mt-20">
+                  <button
+                    class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                    onClick={() => {
+                      handleEdit(user.id);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    class="    text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                    onClick={() => {
+                      handleDelete(user.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      <div className="pagination Buttons">
-        <button className="Button" onClick={handlePrev} disabled={page === 1}>
-          prev
+
+      <div className=" Buttons flex flex-row gap-96  items-center justify-center mt-10 p-6">
+        <button
+          className="Button bg-white px-3 text-green-500 border-none font-semibold rounded-lg py-1 "
+          onClick={handlePrev}
+          disabled={page === 1}
+        >
+          Prev
         </button>
         <button
-          className="Button"
+          className="Button bg-white px-3 text-green-500 border-none font-semibold rounded-lg py-1 "
           onClick={handleNext}
           disabled={page === totalPages}
         >
